7efc3f6abe81289f11e6b0a58788c423b49c0062
TiilS <75080702+TiilS@users.noreply.github.com>
2024-03-25 11:05:48 +0300

    mozilla format

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,8 +8,7 @@ int main(){
     std::cout << "Hello world from " << name << std::endl; // вывод сообщения.
     return 0;
 =======
-int
-main()
+int main()
 {
   std::string name; // инициализация переменной
   std::cin >> name; // получение значение переменной из стандартного потока ввода
