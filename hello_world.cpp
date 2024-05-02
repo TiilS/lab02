513b65a4a286876ccb52b259ca32054d4b7cc6dd
TiilS <ilyaagildin@yandex.ru>
2024-05-02 23:45:06 +0300

    mozilla codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    std::string name; // name
+    string name; // name
     std::cin >> name; // input
     std::cout << "Hello world from " << name << std::endl; // output
     return 0;
